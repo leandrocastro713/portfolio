832aa33f67d53d64ac9a3c13770d701722e2bd58
Leandro Castro <leandro.713@hotmail.com>
2022-11-22 21:48:50 -0300

    mouse enter

--- js/script.js
+++ js/script.js
@@ -6,70 +6,64 @@ let skillPhotoshop = document.getElementById("photoshop-skill");
 let skillAfter = document.getElementById("after-skill");
 let skillPremiere = document.getElementById("premiere-skill");
 
-console.log(bkgSkills)
+// console.log(bkgSkills)
 
 skillHtml.addEventListener("mouseenter",()=> {
     console.log('passou no skill html');
     bkgSkills.style.backgroundBlendMode="lighten";
-    bkgSkills.style.backgroundImage="linear-gradient(white, blue) ,url('./img/html-5.png')";
+    bkgSkills.classList.add("skills-change-html");
+    // bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, #3aaaff) ,url('./img/html-5.png')";
+    console.log(bkgSkills.classList);
 })
-skillPhotoshop.addEventListener("mouseleave",()=> {
+skillHtml.addEventListener("mouseleave",()=> {
     console.log('saiu do html');
     bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, var(--v-backgroung-color))";
-
 })
 
 skillCss.addEventListener("mouseenter",()=> {
     console.log('passou no skill cs');
     bkgSkills.style.backgroundBlendMode="lighten";
-    bkgSkills.style.backgroundImage="linear-gradient(white, blue) ,url('./img/css-3.png')";
+    bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, #3aaaff) ,url('./img/css-3.png')";
 })
-skillPhotoshop.addEventListener("mouseleave",()=> {
+skillCss.addEventListener("mouseleave",()=> {
     console.log('saiu do css');
     bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, var(--v-backgroung-color))";
-
 })
 
 skillJs.addEventListener("mouseenter",()=> {
     console.log('passou no skill js');
     bkgSkills.style.backgroundBlendMode="lighten";
-    bkgSkills.style.backgroundImage="linear-gradient(white, blue) ,url('./img/js.png')";
+    bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, #3aaaff) ,url('./img/js.webp')";
 })
 skillJs.addEventListener("mouseleave",()=> {
     console.log('saiu do js');
     bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, var(--v-backgroung-color))";
-
 })
-
-
-
 
 skillPhotoshop.addEventListener("mouseenter",()=> {
     console.log('passou no skill html');
     bkgSkills.style.backgroundBlendMode="lighten";
-    bkgSkills.style.backgroundImage="linear-gradient(white, blue) ,url('./img/photoshop.jpg')";
+    bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, #3aaaff) ,url('./img/photoshop.jpg')";
 })
 skillPhotoshop.addEventListener("mouseleave",()=> {
     console.log('saiu do html');
     bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, var(--v-backgroung-color))";
-
 })
 
 skillAfter.addEventListener("mouseenter",()=> {
     console.log('passou no after html');
-    bkgSkills.style.backgroundBlendMode="lighten";
-    bkgSkills.style.backgroundImage="linear-gradient(white, blue) ,url('./img/after.jpg')";
+    bkgSkills.style.backgroundBlendMode="darken";
+    bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, #3aaaff), url('./img/after.jpg')";
 })
 skillAfter.addEventListener("mouseleave",()=> {
     console.log('saiu do after');
     bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, var(--v-backgroung-color))";
-
 })
 
 skillPremiere.addEventListener("mouseenter",()=> {
     console.log('passou no premiere');
     bkgSkills.style.backgroundBlendMode="lighten";
-    bkgSkills.style.backgroundImage="linear-gradient(white, blue) ,url('./img/premiere.webp')";
+    bkgSkills.style.backgroundImage="linear-gradient(180deg,#143752, #3aaaff) ,url('./img/premiere.webp')";
 })
 skillPremiere.addEventListener("mouseleave",()=> {
     console.log('saiu do premiere');
